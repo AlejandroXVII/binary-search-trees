6d7f762a7ace7ea02dc9bf81f5dce90e5caa3ae1
AlejandroXVII <m.alejandro.xvii@gmail.com>
2024-01-05 01:28:18 -0400

    Add height, depth, isBalanced & rebalance
    Add these 3 function to the methods of the tree class

--- file.js
+++ file.js
@@ -42,7 +42,23 @@ function mergeSort(array) {
 		return merge(leftArray, rightArray);
 	}
 }
-
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+	if (node === null) {
+		return;
+	}
+	if (node.right !== null) {
+		prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+	}
+	console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+	if (node.left !== null) {
+		prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+	}
+};
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min) + min);
+}
 class Node {
 	constructor(value) {
 		this.value = value;
@@ -80,6 +96,21 @@ class Tree {
 				return this.find(value, node.right);
 			}
 		}
+	}
+	findDistance(value, node = this.root, i = 0) {
+		if (node === null) {
+			return null;
+		} else if (node.value === value) {
+			console.log(i);
+			return i;
+		} else {
+			if (value < node.value) {
+				this.findDistance(value, node.left, (i += 1));
+			} else {
+				this.findDistance(value, node.right, (i += 1));
+			}
+		}
+		return;
 	}
 	insert(value, ptr = this.root) {
 		if (value === ptr.value) {
@@ -171,7 +202,7 @@ class Tree {
 			if (typeof callback === "function") {
 				return;
 			} else {
-				console.log(callback);
+				return callback;
 			}
 		} else {
 			if (typeof callback === "function") {
@@ -187,38 +218,142 @@ class Tree {
 				array.push(array[0].right);
 			}
 			array.shift();
-			//this.levelOrder(array /*, callback*/);
 			this.levelOrder(array, callback);
+			if (typeof callback !== "function") {
+				return callback;
+			}
 		}
 	}
+	preOrder(node = this.root, callback = [], last = true) {
+		if (typeof callback === "function") {
+			callback(node);
+		} else {
+			callback.push(node.value);
+		}
+
+		if (node.left !== null) {
+			this.preOrder(node.left, callback, false);
+		}
+		if (node.right !== null) {
+			this.preOrder(node.right, callback, false);
+		}
+
+		if (last & (typeof callback !== "function")) {
+			return callback;
+		}
+	}
+	inOrder(node = this.root, callback = [], last = true) {
+		if (node.left !== null) {
+			this.inOrder(node.left, callback, false);
+		}
+
+		if (typeof callback === "function") {
+			callback(node);
+		} else {
+			callback.push(node.value);
+		}
+
+		if (node.right !== null) {
+			this.inOrder(node.right, callback, false);
+		}
+
+		if (last & (typeof callback !== "function")) {
+			return callback;
+		}
+	}
+	postOrder(node = this.root, callback = [], last = true) {
+		if (node.left !== null) {
+			this.postOrder(node.left, callback, false);
+		}
+
+		if (node.right !== null) {
+			this.postOrder(node.right, callback, false);
+		}
+
+		if (typeof callback === "function") {
+			callback(node);
+		} else {
+			callback.push(node.value);
+		}
+
+		if (last & (typeof callback !== "function")) {
+			return callback;
+		}
+	}
+	checkBalance(node = this.root, i = 0, array = []) {
+		if ((node.left == null) | (node.right == null)) {
+			array.push(i);
+			console.log(node.value + ": " + i);
+			if ((node.left == null) & (node.right == null)) {
+				return;
+			} else if (node.left == null) {
+				this.checkBalance(node.right, (i += 1), array);
+			} else if (node.right == null) {
+				this.checkBalance(node.left, (i += 1), array);
+			}
+			return array; //*
+		} else {
+			this.checkBalance(node.left, i, array);
+			this.checkBalance(node.right, i, array);
+			return array;
+		}
+	}
+	isBalanced() {
+		const balanceArray = this.checkBalance();
+		let disbalance = balanceArray.find((element) => element >= 2);
+		if (disbalance > 1) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	height(nodeStar = this.root, levelOrderArray = this.levelOrder()) {
+		let lastLeft = levelOrderArray[levelOrderArray.length - 1];
+		this.findDistance(lastLeft, nodeStar);
+	}
+	depth(value) {
+		return this.findDistance(value);
+	}
+	rebalance() {
+		let inOrderArray = this.inOrder();
+		this.root = this.buildTree(inOrderArray, 0, inOrderArray.length - 1);
+	}
 }
-console.log("1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324,");
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-	if (node === null) {
-		return;
-	}
-	if (node.right !== null) {
-		prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-	}
-	console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-	if (node.left !== null) {
-		prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-	}
-};
+
+arrayOfRandoms = [];
+for (let index = 0; index < 99; index++) {
+	arrayOfRandoms.push(getRandomInt(0, 1000));
+}
+console.log(arrayOfRandoms);
+const tree = new Tree(arrayOfRandoms);
+prettyPrint(tree.root);
+/*
 tree.insert(2);
 tree.insert(4.5);
 tree.insert(21);
 tree.insert(8);
 tree.insert(8.5);
 tree.insert(0.1);
-tree.delate(5);
+tree.insert(100);
+tree.insert(7.5);
+tree.insert(16);
+tree.delate(21);
 tree.insert(17);
+tree.insert(18);
+tree.insert(19);
+tree.insert(17.5);
 prettyPrint(tree.root);
 console.log(tree.find(1));
-tree.levelOrder(
+tree
+	.levelOrder /*
 	[tree.root],
 	(randomFunction = (a) => {
 		console.log("( " + a.value + " )");
-	})
-);
+	})*/
+/*();
+console.log(tree.inOrder());
+console.log(tree.preOrder());
+console.log(tree.postOrder());
+console.log(tree.depth(17));
+tree.rebalance();
+prettyPrint(tree.root);*/
